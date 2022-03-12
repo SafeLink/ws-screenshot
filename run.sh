c42781fe2b1b68f67d2c62fad2052d66b681d7c7
kaj <40004347+KAJdev@users.noreply.github.com>
2022-03-12 13:18:13 -0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-###RUN ON PORT 3000
-node index.js -r ./ -p 3000 --oc 1
+###RUN ON PORT 80
+node index.js -r ./ -p 80 --oc 1
 
 ###RUN ON PORT 80 + 443 with AutoSSL
 #node index.js -r ./ -p 80 --ssl --sslport 443 --ssldomain www.yourdomain.com --oc 1
